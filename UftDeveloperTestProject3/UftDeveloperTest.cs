77fd88b3f9a2136d577f45901f27ae1a48d89eb4
Lynn <Lynn@example.com>
2020-07-03 09:49:33 +0800

    add leanft solution

---
+++ UftDeveloperTestProject3/UftDeveloperTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HP.LFT.SDK;
+using HP.LFT.Verifications;
+using HP.LFT.SDK.Web;
+
+namespace UftDeveloperTestProject3
+{
+    [TestClass]
+    public class UftDeveloperTest : UnitTestClassBase<UftDeveloperTest>
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            GlobalSetup(context);
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+//
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var browser = BrowserFactory.Launch(BrowserType.InternetExplorer);
+
+            var browser1 = BrowserFactory.Launch(BrowserType.InternetExplorer);
+
+            var enterYourSearchTermEditField = browser.Describe<IEditField>(new EditFieldDescription
+            {
+                Name = @"q",
+                Placeholder = string.Empty,
+                TagName = @"INPUT",
+                Type = @"search"
+            });
+            enterYourSearchTermEditField.SetValue("hello");
+
+            var webSearchButton = browser.Describe<IButton>(new ButtonDescription
+            {
+                ButtonType = @"submit",
+                Name = @"web search",
+                TagName = @"BUTTON"
+            });
+            webSearchButton.Click();
+
+            var browser2 = BrowserFactory.Launch(BrowserType.InternetExplorer);
+
+            var browser3 = BrowserFactory.Launch(BrowserType.InternetExplorer);
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            GlobalTearDown();
+        }
+    }
+}
